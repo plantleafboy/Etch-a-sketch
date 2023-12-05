e7742e99eb8ca23814aaa74807e5a93d441b3d0c
plantleafboy <alexhpcp@gmail.com>
2023-12-06 03:57:44 +1300

    finish rainbow pen, eraser, grid generator

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 
-const DEFAULTSIZE = 5;
+const DEFAULTSIZE = 16;
 const gridContainer = document.querySelector('.flex-container');
-const mouseFunction = "standard";
+let mouseHovFunction = "black";
 const resetButton = document.querySelector('.reset-grid');
 const rainbowButton = document.querySelector('.rainbow-button');
+const generateButton = document.querySelector('.generate-button');
+const gridInput = document.querySelector('.grid-input');
+const eraserButton = document.querySelector('.eraser-button');
+const favColor = document.querySelector('.fav-color');
 
-const rainbowColours = Math.floor(Math.random() * 256);
+rainbowButton.addEventListener('click', () => {
+    mouseHovFunction = 'rainbow';
+    let rainbowColours = [];
+    for (i=0; i<24; i++) {
+        rainbowColours.push(Math.floor(Math.random()*16777215)).toString(16);
+    }
+});
+
+function generateRainbow() {    
+    let newColour = Math.floor(Math.random()*16777215).toString(16);
+    return `#${newColour}`;
+}
 // const color = `rgb(${red}, ${green}, ${blue})`;
 
+
+favColor.addEventListener('input', (e) => {
+    mouseHovFunction = e.target.value;
+});
+
+eraserButton.addEventListener('click', () => {
+    mouseHovFunction = 'white';
+});
+
+generateButton.addEventListener('click', () => {
+    let childrenArray = Array.from(gridContainer.children);
+
+    childrenArray.forEach((child, index) => {
+        child.remove();
+    });
+
+    createGrid((gridInput.value));
+    gridInput.value = '';
+});
 
 resetButton.addEventListener('click', () => {
     let childrenArray = Array.from(gridContainer.children);
@@ -28,9 +62,21 @@ function createGrid(size) {
         var aDiv = document.createElement('div');
         aDiv.setAttribute('class', 'flex-element');
         aDiv.setAttribute('data-div-number', `div: ${i}`); 
+        console.log()
+        var itemSize = 100 / size;
+        aDiv.style.width = itemSize + '%';
+        aDiv.style.height = itemSize + '%';
+        
         aDiv.addEventListener('mouseover', (e) => {
-            e.target.style.backgroundColor = 'black';
-            console.log(e.target.getAttribute('data-div-number'));
+            if (mouseHovFunction == 'rainbow') {
+                // console.log('rainbow');
+                e.target.style.backgroundColor = generateRainbow();
+            }
+            else {
+                e.target.style.backgroundColor = `${mouseHovFunction}`;
+                // console.log(e.target.getAttribute('data-div-number'));
+            }
+            
         });
         gridContainer.appendChild(aDiv);
     }
